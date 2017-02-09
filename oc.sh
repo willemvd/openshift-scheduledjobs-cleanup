7856bd44afa80c7e3006921476442e9361519c07
willemvd <willem@opencirclesolutions.nl>
2017-02-09 11:54:42 +0100

    add default namespace and serviceaccount to grant access  (oc policy add-role-to-user edit -z scheduled-jobs-cleanup)

--- oc.sh
+++ oc.sh
@@ -4,10 +4,12 @@ export KUBECONFIG=/tmp/.kube
 
 set -e
 
+DEFAULT_NAMESPACE=$(eval cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+
 oc login https://$KUBERNETES_PORT_443_TCP_ADDR:$KUBERNETES_SERVICE_PORT_HTTPS \
   --token `cat /var/run/secrets/kubernetes.io/serviceaccount/token` \
-  --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
-oc get jobs > /tmp/jobs
+  --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+oc get jobs -n ${DEFAULT_NAMESPACE} > /tmp/jobs
 tail -n +2 /tmp/jobs > /tmp/jobs-without-header
 
 while read JOB_WITH_STATS
@@ -16,7 +18,7 @@ do
     SUCCESSFUL_RUN=$(eval echo "${JOB_WITH_STATS}" | awk '{print $3}')
     if [ ${SUCCESSFUL_RUN} == "0" ]; then
       echo "Successfully ended job ${JOB}, delete it"
-      oc delete job ${JOB}
+      oc delete job ${JOB} -n ${DEFAULT_NAMESPACE}
     else
       echo "${JOB} not successfully ended"
     fi
